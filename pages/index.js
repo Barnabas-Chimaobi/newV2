fe966c7edfa88b85065a5f8641420cf7b88af82b
Matthew Barnabas <enyinnayabarnabas@gmail.com>
2023-09-20 12:28:31 +0100

    added login

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
-import Header from "@/components/header"
+import Header from "@/components/header";
 
 export default function Home() {
-  return (
-    <>
-      <Header>
-
-        <div>Hello</div>
-      </Header>
-    </>
-  )
+	return (
+		<>
+			<Header>
+				<div className="page-wrapper">
+					{/* <div className="content container-fluid"> */}
+					<h1>This is our Landing Page</h1>
+					{/* </div> */}
+				</div>
+			</Header>
+		</>
+	);
 }
