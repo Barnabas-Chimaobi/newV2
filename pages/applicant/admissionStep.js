5420d276cd917fa9c63f5119bcdd4edf134d5fe8
Matthew Barnabas <enyinnayabarnabas@gmail.com>
2023-11-08 16:59:41 +0100

    fixing admission steps

---
+++ pages/applicant/admissionStep.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Admittedstudent from "@/components/admittedstudent";
+import { useSelector } from "react-redux";
+
+export default function AdmissionStep() {
+	const statusState = useSelector((state) => state.invoice.statuses);
+	const statusvalue = statusState.status;
+	return (
+		<div>
+			<Admittedstudent status={statusvalue} formNo={statusState.formNo} />
+		</div>
+	);
+}
