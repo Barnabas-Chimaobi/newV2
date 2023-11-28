10e7d7c3855aafee4fe8f9008b0836dbed281757
Matthew Barnabas <enyinnayabarnabas@gmail.com>
2023-11-28 11:15:40 +0100

    fix issues

--- pages/api/baseurl.js
+++ pages/api/baseurl.js
@@ -1,3 +1,4 @@
 const BaseUrl = "https://backendvirtualschoolv2.lloydant.com/graphql/";
-const FrontBaseUrl = "http://localhost:3000/";
+// const FrontBaseUrl = "http://localhost:3000/";
+const FrontBaseUrl = "new-v2-test.netlify.app/";
 export { BaseUrl, FrontBaseUrl };
