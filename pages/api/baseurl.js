a0f661502c0e625967b23ed9b85bb374529c84f3
Ah-ugo <ahuekweprinceugo@gmail.com>
2024-01-15 14:04:54 +0100

    sdfgfd

--- pages/api/baseurl.js
+++ pages/api/baseurl.js
@@ -1,4 +1,4 @@
 const BaseUrl = "https://backendvirtualschoolv2.lloydant.com/graphql/";
-// const FrontBaseUrl = "http://localhost:3000/";
-const FrontBaseUrl = "new-v2-test.netlify.app/";
+const FrontBaseUrl = "http://localhost:3000/";
+// const FrontBaseUrl = "new-v2-test.netlify.app/";
 export { BaseUrl, FrontBaseUrl };
